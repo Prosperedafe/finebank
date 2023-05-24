feea0f2f6d299a29e09234d7b4b2860d1a582158
prosper edafe <mamusedafe19@gmail.com>
2023-05-24 22:55:23 +0100

    created component

---
+++ src/components/navigation/sidenav.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/FINEbank.IO.svg';
+
+const SideNav = () => {
+    return (
+        <nav>
+            <img src={logo} alt="logo" />
+        </nav>
+    )
+}
+
+export default SideNav;
