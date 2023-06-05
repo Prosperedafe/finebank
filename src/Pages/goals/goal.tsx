3cd1405daf463b896fd1ce451ab62f8bf9e40809
oviesalvay <oviesalvay@gmail.com>
2023-06-05 16:01:42 +0100

    update

--- src/Pages/goals/goal.tsx
+++ src/Pages/goals/goal.tsx
@@ -1,36 +1,36 @@
-import { PagesHeader } from "../../components/dashboard/header";
-  import Housing from './assests/Housing.svg';
-  import food from './assests/food.png';
-  import transportation from './assests/transportation.svg';
-  import icon from './assests/Icon.svg';
-  import shopping from './assests/shopping.svg';
-  import other from './assests/other.svg';
-  import graphed from './assests/graphed.svg';
-  import Graph from './assests/Graph.svg';
-  const goals: Array<object> = [
-    {
-   text: "Housing",
-   img: Housing,
-   price:"$250.00",
-    },
-    {
-        text:"Food",
-        img:food,
-        price:"$250.00",
-    },
-    {
-        text:"Transportation",
-        img:transportation,
-        price:"$250.00",
-    }
+// import { PagesHeader } from "../../components/dashboard/header";
+//   import Housing from './assests/Housing.svg';
+//   import food from './assests/food.png';
+//   import transportation from './assests/transportation.svg';
+//   import icon from './assests/Icon.svg';
+//   import shopping from './assests/shopping.svg';
+//   import other from './assests/other.svg';
+//   import graphed from './assests/graphed.svg';
+//   import Graph from './assests/Graph.svg';
+//   const goals: Array<object> = [
+//     {
+//    text: "Housing",
+//    img: Housing,
+//    price:"$250.00",
+//     },
+//     {
+//         text:"Food",
+//         img:food,
+//         price:"$250.00",
+//     },
+//     {
+//         text:"Transportation",
+//         img:transportation,
+//         price:"$250.00",
+//     }
 
-  ]
-const Goals = () =>{
-    return(
-        <>
-    <PagesHeader />
-    <h1>return</h1>
-    </>
-    )
-}
-export default Goals;
+//   ]
+// const Goals = () =>{
+//     return(
+//         <>
+//     <PagesHeader />
+//     <h1>return</h1>
+//     </>
+//     )
+// }
+// export default Goals;
