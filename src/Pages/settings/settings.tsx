10424adac731ad60da703652d25c6aa4e1d51a7c
oviesalvay <oviesalvay@gmail.com>
2023-06-19 14:24:02 +0100

    settings page completed

---
+++ src/Pages/settings/settings.tsx
@@ -0,0 +1,61 @@
+import { PagesHeader } from '../../components/dashboard/header';
+import { FC, useState } from "react";
+import Picture from './assest/Picture.svg';
+import './setting.css';
+export const Settings: FC = () => {
+
+  const [tab, setTab] = useState<number>(1);
+  return (
+    <>
+      <PagesHeader />
+      <section className='settings'>
+        <div className=''>
+          <hgroup>
+            <button onClick={() => setTab(1)} className={tab === 1 ? 'current' : ''}>Account</button>
+            <button onClick={() => setTab(2)} className={tab === 2 ? 'current' : ''}>Security</button>
+          </hgroup>
+        </div>
+        {
+          tab === 1 &&
+          <div className='settings__account'>
+            <div>
+              <form>
+                <label htmlFor='name' id="name">Full name</label><br></br>
+                <input type="name" id="name" placeholder="Tanzir Rahman" /><br></br>
+                <label htmlFor='email' id="text">Email</label><br></br>
+                <input type="email" id="text" placeholder='tanzir.rahman@email.com' /><br></br>
+                <label htmlFor='name' id="name">Username</label><br></br>
+                <input type="UName" id="name" placeholder='tanzir.rahman' /><br></br>
+                <label htmlFor='number' id="number"></label>Phone Number<br></br>
+                <input type="number" id="number" placeholder='+880  |  51547 58868' />
+              </form>
+              <button className='update'>Update Profile</button>
+            </div>
+            <div className="profile">
+              <h3>Your Profile Picture</h3>
+              <figure>
+                <img src={Picture} alt="" /> 
+              </figure>
+            </div>
+          </div>
+        } 
+        {tab === 2 &&
+          <>
+            <form className='settings__security'>
+              <label htmlFor="text" id="text">Old Password</label><br></br>
+              <input type="Pword" id="text" placeholder='************' /><br></br>
+              <label htmlFor="text" id="text">New Password</label><br></br>
+              <input type="Pword" id="text" placeholder='************' /><br></br>
+              <label htmlFor="text" id="text">Retype Password</label><br></br>
+              <input type="Pword" id="text" placeholder='************' /><br></br>
+              <label htmlFor="number" id="number">Phone Number </label><br></br>
+              <input type="number" id="number" placeholder='+880  |  51547 58868' /><br></br>
+            </form>
+            <button className='update'>Update Password</button>
+          </>
+        }
+      </section>
+    </>
+  )
+}
+export default Settings;
